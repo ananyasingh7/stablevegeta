6fb0117104710d01489644be99eeb35362c10791
Ananya Singh <shtoogienet@gmail.com>
2024-12-18 23:13:20 -0500

    removed an icon or two for now

--- src/App.js
+++ src/App.js
@@ -5,14 +5,8 @@ function App() {
     <div className="min-h-screen flex flex-col items-center justify-center bg-black">
       <h1 className="text-white text-4xl mb-6">Coming Soon</h1>
       <div className="flex space-x-4 text-white text-2xl">
-        <a href="https://x.com/stablevegeta" target="_blank" rel="noopener noreferrer" className="hover:text-gray-300 transition">
+        <a href="https://x.com/TheStableVegeta" target="_blank" rel="noopener noreferrer" className="hover:text-gray-300 transition">
           <FaTwitter />
-        </a>
-        <a href="https://linkedin.com/in/ananyasingh7" target="_blank" rel="noopener noreferrer" className="hover:text-gray-300 transition">
-          <FaLinkedin />
-        </a>
-        <a href="https://github.com/ananyasingh7" target="_blank" rel="noopener noreferrer" className="hover:text-gray-300 transition">
-          <FaGithub />
         </a>
       </div>
     </div>
